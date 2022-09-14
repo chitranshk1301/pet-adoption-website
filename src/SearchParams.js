716c9a461d0f7bc63286805376e8ffb92d0b9278
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2022-09-14 18:08:41 +0530

    new commit

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -20,7 +20,7 @@ const SearchParams = () => {
 
   async function requestPets(){
     const res = await fetch(
-      `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
     const json = await res.json();
     setPets(json.pets);
